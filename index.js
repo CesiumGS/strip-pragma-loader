5e1ac042b2059bc3ed56fdd486873fe756d7228b
ggetz <ggetz881@gmail.com>
2017-10-12 10:57:03 -0400

    Fomratting, cleanup package.json

--- index.js
+++ index.js
@@ -2,37 +2,37 @@
 
 var loaderUtils = require('loader-utils');
 
-function constructRegex (pragma, exclusive) {
-	var prefix = exclusive ? 'exclude' : 'include';
-	var s = '[\\t ]*\\/\\/>>\\s?' +
-		prefix +
-		'Start\\s?\\(\\s?(["\'])' +
-		pragma +
-		'\\1\\s?,\\s?pragmas\\.' + 
-		pragma + 
-		'\\s?\\)\\s?;?' +
+function constructRegex(pragma, exclusive) {
+    var prefix = exclusive ? 'exclude' : 'include';
+    var s = '[\\t ]*\\/\\/>>\\s?' +
+        prefix +
+        'Start\\s?\\(\\s?(["\'])' +
+        pragma +
+        '\\1\\s?,\\s?pragmas\\.' +
+        pragma +
+        '\\s?\\)\\s?;?' +
 
-		// multiline code block
-		'[\\s\\S]*?' +
+        // multiline code block
+        '[\\s\\S]*?' +
 
-		// end comment
-		'[\\t ]*\\/\\/>>\\s?' + 
-		prefix + 
-		'End\\s?\\(\\s?(["\'])' +
-		pragma +
-		'\\2\\s?\\)\\s?;?\\s?[\\t ]*\\n?';
+        // end comment
+        '[\\t ]*\\/\\/>>\\s?' +
+        prefix +
+        'End\\s?\\(\\s?(["\'])' +
+        pragma +
+        '\\2\\s?\\)\\s?;?\\s?[\\t ]*\\n?';
 
-	return new RegExp(s, 'gm');
+    return new RegExp(s, 'gm');
 }
 
-function stripPragmas (context, source) {
+function stripPragmas(context, source) {
     var options = loaderUtils.getOptions(context) || {};
     var pragmas = options.pragmas || {};
 
     for (var key in pragmas) {
-    	if (pragmas.hasOwnProperty(key)) {
-			source = source.replace(constructRegex(key, pragmas[key]), '');
-		}
+        if (pragmas.hasOwnProperty(key)) {
+            source = source.replace(constructRegex(key, pragmas[key]), '');
+        }
     }
 
     if (context.cacheable) {
@@ -43,5 +43,5 @@ function stripPragmas (context, source) {
 }
 
 module.exports = function (source, map) {
-	this.callback(null, stripPragmas(this, source), map);
+    this.callback(null, stripPragmas(this, source), map);
 };
